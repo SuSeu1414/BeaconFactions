f7f6c146fccb9c05adc1c6defa57f584ea253624
BuildTools <szymonwitek02@gmail.com>
2020-03-30 00:33:46 +0200

    Add UUID to User class

--- src/main/java/pl/suseu/bfactions/base/user/User.java
+++ src/main/java/pl/suseu/bfactions/base/user/User.java
@@ -1,4 +1,16 @@
 package pl.suseu.bfactions.base.user;
 
+import java.util.UUID;
+
 public class User {
+
+    private UUID uuid;
+
+    public User(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
 }
