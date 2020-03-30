ab79634e0027d3bd7a59d12d9e2a2325574cf657
BuildTools <szymonwitek02@gmail.com>
2020-03-30 21:47:28 +0200

    Add getters/setters to Guild class

--- src/main/java/pl/suseu/bfactions/base/guild/Guild.java
+++ src/main/java/pl/suseu/bfactions/base/guild/Guild.java
@@ -2,14 +2,15 @@ package pl.suseu.bfactions.base.guild;
 
 import pl.suseu.bfactions.base.user.User;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Guild {
 
-    private final String name;
-    private final User owner;
+    private String name;
+    private User owner;
 
     private final Set<User> members = ConcurrentHashMap.newKeySet();
     private final Map<User, GuildPermissionSet> permissions = new ConcurrentHashMap<>();
@@ -17,5 +18,62 @@ public class Guild {
     public Guild(String name, User owner) {
         this.name = name;
         this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Set<User> getMembers() {
+        return new HashSet<>(this.members);
+    }
+
+    public void addMember(User user) {
+        this.members.add(user);
+        // todo add default permission set to permissions map
+    }
+
+    public void removeMember(User user) {
+        this.members.remove(user);
+        this.permissions.remove(user);
+    }
+
+    public void addMemberPermission(User member, GuildPermission permission) {
+        GuildPermissionSet permissionSet = this.permissions.get(member);
+        if (permissionSet == null) {
+            return;
+        }
+
+        permissionSet.addPermission(permission);
+    }
+
+    public void removeMemberPermission(User member, GuildPermission permission) {
+        GuildPermissionSet permissionSet = this.permissions.get(member);
+        if (permissionSet == null) {
+            return;
+        }
+
+        permissionSet.removePermission(permission);
+    }
+
+    public boolean hasPermission(User member, GuildPermission permission) {
+        GuildPermissionSet permissionSet = this.permissions.get(member);
+        if (permissionSet == null) {
+            return false;
+        }
+
+        return permissionSet.hasPermission(permission);
     }
 }
