099cf83f98af0df8753ae87ea5e1b100e7221187
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-03-30 13:12:49 +0200

    implement configuration base

--- src/main/java/pl/suseu/bfactions/BFactions.java
+++ src/main/java/pl/suseu/bfactions/BFactions.java
@@ -1,11 +1,29 @@
 package pl.suseu.bfactions;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.suseu.bfactions.settings.Settings;
+
+import java.util.logging.Logger;
 
 public class BFactions extends JavaPlugin {
 
+    private Settings settings;
+    private Logger log;
+
     @Override
     public void onEnable() {
+        settings = new Settings(this);
 
+        if (!settings.loadConfig()) {
+            log.severe("Invalid config, check previous warnings!");
+            log.severe("Disabling plugin...");
+
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+    }
+
+    public Settings getSettings() {
+        return settings;
     }
 }
---
+++ src/main/java/pl/suseu/bfactions/settings/Settings.java
@@ -0,0 +1,139 @@
+package pl.suseu.bfactions.settings;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import pl.suseu.bfactions.BFactions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class Settings {
+
+    public int guildNameMaxLength;
+    public int guildNameMinLength;
+    public int guildTagMaxLength;
+    public int guildTagMinLength;
+    public int guildMembersMax;
+    public int cuboidSizeInitial;
+    public int cuboidDistanceMin;
+    public double fieldEnergyInitial;
+    public double fieldPassiveDrainAmount;
+    public int fieldPassiveDrainDelay;
+    public double fieldDamageArrow;
+    public double fieldDamageTNT;
+    public Map<Material, Double> fieldEnergyConversions = new HashMap<>();
+    private BFactions plugin;
+    private FileConfiguration cfg;
+    private Logger log;
+
+    public Settings(BFactions plugin) {
+        this.plugin = plugin;
+        this.cfg = plugin.getConfig();
+        this.log = plugin.getLogger();
+    }
+
+    public boolean loadConfig() {
+        if (!checkIntegrity()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean checkIntegrity() {
+        boolean success = true;
+
+        if (!cfg.isConfigurationSection("guild")) {
+            log.warning("Configuration: Missing 'guild' section!");
+            success = false;
+        } else {
+            ConfigurationSection section = cfg.getConfigurationSection("guild");
+
+            if (!section.isInt("name-max-length")) {
+                log.warning("Configuration (guild): Missing/Invalid 'name-max-length' entry!");
+                success = false;
+            }
+            if (!section.isInt("name-min-length")) {
+                log.warning("Configuration (guild): Missing/Invalid 'name-min-length' entry!");
+                success = false;
+            }
+            if (!section.isInt("tag-max-length")) {
+                log.warning("Configuration (guild): Missing/Invalid 'tag-max-length' entry!");
+                success = false;
+            }
+            if (!section.isInt("tag-min-length")) {
+                log.warning("Configuration (guild): Missing/Invalid 'tag-min-length' entry!");
+                success = false;
+            }
+            if (!section.isInt("max-members")) {
+                log.warning("Configuration (guild): Missing/Invalid 'max-members' entry!");
+                success = false;
+            }
+        }
+
+        if (!cfg.isConfigurationSection("cuboid")) {
+            log.warning("Configuration: Missing 'cuboid' section!");
+            success = false;
+        } else {
+            ConfigurationSection section = cfg.getConfigurationSection("cuboid");
+
+            if (!section.isInt("initial-size")) {
+                log.warning("Configuration (cuboid): Missing/Invalid 'initial-size' entry!");
+                success = false;
+            }
+            if (!section.isInt("minimum-distance")) {
+                log.warning("Configuration (cuboid): Missing/Invalid 'minimum-distance' entry!");
+                success = false;
+            }
+        }
+
+        if (!cfg.isConfigurationSection("field")) {
+            log.warning("Configuration: Missing 'field' section!");
+            success = false;
+        } else {
+            ConfigurationSection section = cfg.getConfigurationSection("field");
+
+            if (!section.isDouble("initial-energy")) {
+                log.warning("Configuration (field): Missing/Invalid 'initial-energy' entry!");
+                success = false;
+            }
+            if (!section.isDouble("passive-drain-amount")) {
+                log.warning("Configuration (field): Missing/Invalid 'passive-drain-amount' entry!");
+                success = false;
+            }
+            if (!section.isInt("passive-drain-delay")) {
+                log.warning("Configuration (field): Missing/Invalid 'passive-drain-delay' entry!");
+                success = false;
+            }
+            if (!section.isDouble("arrow-damage")) {
+                log.warning("Configuration (field): Missing/Invalid 'arrow-damage' entry!");
+                success = false;
+            }
+            if (!section.isDouble("tnt-damage")) {
+                log.warning("Configuration (field): Missing/Invalid 'tnt-damage' entry!");
+                success = false;
+            }
+
+            if (!section.isConfigurationSection("energy-fuel")) {
+                log.warning("Configuration (field): Missing 'energy-fuel' section!");
+                success = false;
+            } else {
+                ConfigurationSection conversionsSection = section.getConfigurationSection("energy-fuel");
+
+                for (String materialName : conversionsSection.getKeys(false)) {
+                    Material material = Material.getMaterial(materialName);
+
+                    if (material == null || !conversionsSection.isDouble(materialName)) {
+                        log.warning("Configuration (field.energy-fuel): Invalid '" + materialName + "'");
+                        success = false;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return success;
+    }
+}
