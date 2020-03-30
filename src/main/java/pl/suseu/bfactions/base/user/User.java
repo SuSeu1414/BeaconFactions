367084621d1e1ecf34c3cedc4ef63d3ef2eef0a2
BuildTools <szymonwitek02@gmail.com>
2020-03-30 21:21:10 +0200

    Add guilds set to User class

--- src/main/java/pl/suseu/bfactions/base/user/User.java
+++ src/main/java/pl/suseu/bfactions/base/user/User.java
@@ -1,10 +1,16 @@
 package pl.suseu.bfactions.base.user;
 
+import pl.suseu.bfactions.base.guild.Guild;
+
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class User {
 
     private UUID uuid;
+
+    private Set<Guild> guilds = ConcurrentHashMap.newKeySet();
 
     public User(UUID uuid) {
         this.uuid = uuid;
