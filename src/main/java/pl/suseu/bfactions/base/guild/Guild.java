5a588ab3251c937f6127e305ca1a085ad68f07d0
BuildTools <szymonwitek02@gmail.com>
2020-03-30 21:22:42 +0200

    Add basic fields to Guild class

--- src/main/java/pl/suseu/bfactions/base/guild/Guild.java
+++ src/main/java/pl/suseu/bfactions/base/guild/Guild.java
@@ -1,4 +1,15 @@
 package pl.suseu.bfactions.base.guild;
 
+import pl.suseu.bfactions.base.user.User;
+
+import java.util.Map;
+import java.util.Set;
+
 public class Guild {
+
+    private String name;
+    private User owner;
+    private Set<User> members;
+    private Map<User, GuildPermissionSet> permissions;
+
 }
