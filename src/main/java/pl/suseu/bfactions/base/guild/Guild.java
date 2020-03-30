06bffe671d9ebacd6148b50e549b7fc227a8d04a
BuildTools <szymonwitek02@gmail.com>
2020-03-30 21:23:06 +0200

    Initialize map in Guild class

--- src/main/java/pl/suseu/bfactions/base/guild/Guild.java
+++ src/main/java/pl/suseu/bfactions/base/guild/Guild.java
@@ -4,12 +4,13 @@ import pl.suseu.bfactions.base.user.User;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Guild {
 
     private String name;
     private User owner;
     private Set<User> members;
-    private Map<User, GuildPermissionSet> permissions;
+    private Map<User, GuildPermissionSet> permissions = new ConcurrentHashMap<>();
 
 }
