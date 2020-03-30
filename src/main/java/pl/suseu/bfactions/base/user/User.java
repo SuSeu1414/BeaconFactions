b175eda99a5915107acb70fb84054394df11b8e4
BuildTools <szymonwitek02@gmail.com>
2020-03-30 21:53:05 +0200

    Add guild methods to User class

--- src/main/java/pl/suseu/bfactions/base/user/User.java
+++ src/main/java/pl/suseu/bfactions/base/user/User.java
@@ -2,6 +2,7 @@ package pl.suseu.bfactions.base.user;
 
 import pl.suseu.bfactions.base.guild.Guild;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -18,5 +19,30 @@ public class User {
 
     public UUID getUuid() {
         return uuid;
+    }
+
+    public boolean ownsGuild(){
+        return this.getOwnedGuild() != null;
+    }
+
+    public Guild getOwnedGuild() {
+        for (Guild guild : this.guilds) {
+            if (guild.getOwner().equals(this)) {
+                return guild;
+            }
+        }
+        return null;
+    }
+
+    public Set<Guild> getGuilds() {
+        return new HashSet<>(this.guilds);
+    }
+
+    public void addGuild(Guild guild) {
+        this.guilds.add(guild);
+    }
+
+    public void removeGuild(Guild guild) {
+        this.guilds.remove(guild);
     }
 }
