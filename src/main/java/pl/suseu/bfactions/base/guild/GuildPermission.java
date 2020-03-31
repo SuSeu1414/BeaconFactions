7181802878a0c7c5105ccef086951b3b9ab0860c
BuildTools <szymonwitek02@gmail.com>
2020-03-31 11:46:40 +0200

    Add better calculation of guild permissions

--- src/main/java/pl/suseu/bfactions/base/guild/GuildPermission.java
+++ src/main/java/pl/suseu/bfactions/base/guild/GuildPermission.java
@@ -2,8 +2,28 @@ package pl.suseu.bfactions.base.guild;
 
 public enum GuildPermission {
 
-    MODIFY_TERRAIN,
-    OPEN_CHESTS,
-    INVITE_MEMBERS
+    MANAGE(1),
+    OPEN_CHESTS(2),
+    OPEN_DOORS(4),
+    MODIFY_TERRAIN(8),
+    KILL_ANIMALS(16);
 
+    final int bit;
+
+    GuildPermission(int bit) {
+        this.bit = bit;
+    }
+
+    public int getBit() {
+        return bit;
+    }
+
+    public static GuildPermission getByBit(int bit) {
+        for (GuildPermission permission : GuildPermission.values()) {
+            if (permission.getBit() == bit) {
+                return permission;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/pl/suseu/bfactions/base/guild/GuildPermissionSet.java
+++ src/main/java/pl/suseu/bfactions/base/guild/GuildPermissionSet.java
@@ -5,7 +5,22 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class GuildPermissionSet {
 
+    private static final int MAX_BITS = 10;
+
+
     private Set<GuildPermission> permissions = ConcurrentHashMap.newKeySet();
+
+    public GuildPermissionSet(int data) {
+        for (int i = 0; i <= MAX_BITS; i++) {
+            int bit = data &= (0b1 << i);
+            GuildPermission permission = GuildPermission.getByBit(bit);
+            if (permission == null) {
+                continue;
+            }
+
+            this.permissions.add(permission);
+        }
+    }
 
     public void addPermission(GuildPermission permission) {
         permissions.add(permission);
@@ -18,5 +33,14 @@ public class GuildPermissionSet {
     public boolean hasPermission(GuildPermission permission) {
         return permissions.contains(permission);
     }
+
+    public int serialize() {
+        int data = 0;
+        for (GuildPermission perm : this.permissions) {
+            data |= perm.getBit();
+        }
+        return data;
+    }
+
 
 }
