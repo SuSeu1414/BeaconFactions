ac947242fb975f22b0ef0326865b02ff3db8520e
BuildTools <szymonwitek02@gmail.com>
2020-03-31 21:32:56 +0200

    Add database/repository instances and getters to main class

--- src/main/java/pl/suseu/bfactions/BFactions.java
+++ src/main/java/pl/suseu/bfactions/BFactions.java
@@ -2,6 +2,10 @@ package pl.suseu.bfactions;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.suseu.bfactions.base.guild.GuildRepository;
+import pl.suseu.bfactions.base.region.RegionRepository;
+import pl.suseu.bfactions.base.user.UserRepository;
+import pl.suseu.bfactions.database.Database;
 import pl.suseu.bfactions.settings.Settings;
 
 import java.util.logging.Logger;
@@ -9,7 +13,12 @@ import java.util.logging.Logger;
 public class BFactions extends JavaPlugin {
 
     private Settings settings;
+    private Database database;
     private Logger log;
+
+    private UserRepository userRepository;
+    private RegionRepository regionRepository;
+    private GuildRepository guildRepository;
 
     @Override
     public void onEnable() {
@@ -27,5 +36,21 @@ public class BFactions extends JavaPlugin {
 
     public Settings getSettings() {
         return settings;
+    }
+
+    public Database getDatabase() {
+        return database;
+    }
+
+    public GuildRepository getGuildRepository() {
+        return guildRepository;
+    }
+
+    public RegionRepository getRegionRepository() {
+        return regionRepository;
+    }
+
+    public UserRepository getUserRepository() {
+        return userRepository;
     }
 }
