e0c23403f694390118f2af7d63f2b3ad05e0895e
BuildTools <szymonwitek02@gmail.com>
2020-03-31 21:34:33 +0200

    Implement UserDataController base

--- src/main/java/pl/suseu/bfactions/base/user/UserDataController.java
+++ src/main/java/pl/suseu/bfactions/base/user/UserDataController.java
@@ -1,10 +1,19 @@
 package pl.suseu.bfactions.base.user;
 
 import pl.suseu.bfactions.BFactions;
+import pl.suseu.bfactions.database.Database;
 
 public class UserDataController {
 
     private BFactions plugin;
+    private Database database;
 
+    public UserDataController(BFactions plugin) {
+        this.plugin = plugin;
+        this.database = plugin.getDatabase();
+    }
 
+    public boolean loadUsers() {
+        return true;
+    }
 }
