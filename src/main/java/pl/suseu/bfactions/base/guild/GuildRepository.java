1369e11754fe383d37ad8a0cd8affb586614bed9
BuildTools <szymonwitek02@gmail.com>
2020-04-01 23:04:59 +0200

    Create GuildRepository constructor

--- src/main/java/pl/suseu/bfactions/base/guild/GuildRepository.java
+++ src/main/java/pl/suseu/bfactions/base/guild/GuildRepository.java
@@ -1,4 +1,12 @@
 package pl.suseu.bfactions.base.guild;
 
+import pl.suseu.bfactions.BFactions;
+
 public class GuildRepository {
+
+    private BFactions plugin;
+
+    public GuildRepository(BFactions plugin) {
+        this.plugin = plugin;
+    }
 }
