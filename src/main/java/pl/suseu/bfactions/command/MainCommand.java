ab79a4f60e21bdfa81c05a87d32117e2231f7a36
BuildTools <szymonwitek02@gmail.com>
2020-04-01 19:46:39 +0200

    Implement MainCommand

---
+++ src/main/java/pl/suseu/bfactions/command/MainCommand.java
@@ -0,0 +1,53 @@
+package pl.suseu.bfactions.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import pl.rynbou.langapi3.LangAPI;
+import pl.suseu.bfactions.BFactions;
+
+import java.util.Arrays;
+
+public class MainCommand implements CommandExecutor {
+
+    private final BFactions plugin;
+    private final LangAPI lang;
+    private final BCommandMap commandMap;
+
+    public MainCommand(BFactions plugin) {
+        this.plugin = plugin;
+        this.lang = plugin.getLang();
+        this.commandMap = new BCommandMap(plugin);
+        this.commandMap.initCommands();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            lang.sendMessage("base-command-info", sender);
+            return true;
+        }
+
+        BCommand bCommand = commandMap.getCommand(args[0]);
+
+        if (bCommand == null) {
+            lang.sendMessage("unknown-command", sender);
+            return true;
+        }
+
+        if (!sender.hasPermission(bCommand.getPermission())) {
+            lang.sendMessage("no-permission", sender);
+            return true;
+        }
+
+        if (bCommand.isNeedsArguments() && args.length == 1) {
+            String usage = "/" + label + " " + bCommand.getUsage();
+            lang.sendMessage("command-usage", sender, "%usage%", usage);
+            return true;
+        }
+
+        bCommand.getExecutor().execute(sender, bCommand, Arrays.asList(args).subList(1, args.length));
+
+        return true;
+    }
+}
