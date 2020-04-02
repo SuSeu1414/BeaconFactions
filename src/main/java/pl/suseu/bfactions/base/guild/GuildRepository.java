049ceb9edb206c0760842bf0ce51af6847dc5d0b
BuildTools <szymonwitek02@gmail.com>
2020-04-02 02:36:39 +0200

    Implement GuildRepository

--- src/main/java/pl/suseu/bfactions/base/guild/GuildRepository.java
+++ src/main/java/pl/suseu/bfactions/base/guild/GuildRepository.java
@@ -2,11 +2,46 @@ package pl.suseu.bfactions.base.guild;
 
 import pl.suseu.bfactions.BFactions;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class GuildRepository {
 
     private BFactions plugin;
 
+    private final Map<UUID, Guild> guilds = new ConcurrentHashMap<>();
+    private final Set<UUID> modifiedGuilds = ConcurrentHashMap.newKeySet();
+
     public GuildRepository(BFactions plugin) {
         this.plugin = plugin;
+    }
+
+    public void addGuild(Guild guild, boolean isNew) {
+        this.guilds.put(guild.getUuid(), guild);
+        if (isNew) {
+            this.addModifiedGuild(guild);
+        }
+    }
+
+    public Guild getGuild(UUID uuid) {
+        return this.guilds.get(uuid);
+    }
+
+    public void addModifiedGuild(Guild guild) {
+        this.addModifiedGuild(guild.getUuid());
+    }
+
+    public void addModifiedGuild(UUID uuid) {
+        this.modifiedGuilds.add(uuid);
+    }
+
+    /**
+     * @return a copy of guilds set
+     */
+    public Set<Guild> getGuilds() {
+        return new HashSet<>(this.guilds.values());
     }
 }
