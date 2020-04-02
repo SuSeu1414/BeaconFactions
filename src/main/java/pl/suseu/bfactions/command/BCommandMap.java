10274111c2d49ec5f0872dc52cf3e4b6e9685acf
BuildTools <szymonwitek02@gmail.com>
2020-04-02 14:38:16 +0200

    Add test command

--- src/main/java/pl/suseu/bfactions/command/BCommandMap.java
+++ src/main/java/pl/suseu/bfactions/command/BCommandMap.java
@@ -1,6 +1,7 @@
 package pl.suseu.bfactions.command;
 
 import pl.suseu.bfactions.BFactions;
+import pl.suseu.bfactions.command.cmds.TestAddMemberCommandExecutor;
 import pl.suseu.bfactions.command.cmds.TestCommandExecutor;
 import pl.suseu.bfactions.util.StringArrayUtil;
 
@@ -13,11 +14,13 @@ public class BCommandMap {
     private final Set<BCommand> commands = new HashSet<>();
 
     private final TestCommandExecutor testCommandExecutor;
+    private final TestAddMemberCommandExecutor testAddMemberCommandExecutor;
 
     public BCommandMap(BFactions plugin) {
         this.plugin = plugin;
 
         this.testCommandExecutor = new TestCommandExecutor();
+        this.testAddMemberCommandExecutor = new TestAddMemberCommandExecutor(this.plugin);
     }
 
     public void addCommand(BCommand command) {
@@ -40,6 +43,11 @@ public class BCommandMap {
                 .addAlias("t")
                 .addAlias("tst")
                 .setUsage("test <arg>")
+                .build(this.commands);
+        new BCommandBuilder("add")
+                .setExecutor(this.testCommandExecutor)
+                .setNeedsArguments(true)
+                .setUsage("add <player>")
                 .build(this.commands);
     }
 }
