bfb6c160f1dac2dc88e08a3042dbd9b17b8e8b89
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-04-10 17:15:19 +0200

    Apply velocity on every move event in foreign region, teleport to safety if it fails

--- src/main/java/pl/suseu/bfactions/base/field/listener/PlayerMoveInRegionListener.java
+++ src/main/java/pl/suseu/bfactions/base/field/listener/PlayerMoveInRegionListener.java
@@ -19,17 +19,24 @@ public class PlayerMoveInRegionListener implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveInRegionEvent event) {
+
         Player player = event.getPlayer();
         User user = plugin.getUserRepository().getUser(player.getUniqueId());
 
         if (user.getCurrentRegion().getGuild().isMember(user)) {
             return;
         }
+        if (System.currentTimeMillis() - user.getLastRegionChange() > 3000) {
+            user.getCurrentRegion().teleportToSafety(player);
+        }
 
         Location from = event.getRegion().getCenter().clone();
         Location to = event.getFrom();
+        if (!user.getCurrentRegion().isInBorder(from)) {
+            return;
+        }
         from.setY(to.getY());
-        Vector vector = from.toVector().subtract(to.toVector()).multiply(-1).normalize();
+        Vector vector = from.toVector().subtract(to.toVector()).multiply(-1).normalize().multiply(1);
         event.getPlayer().setVelocity(vector);
     }
 }
