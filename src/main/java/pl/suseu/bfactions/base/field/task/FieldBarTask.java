fcdce096838304ada1cba2f8d8c15bd93a0149d6
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-04-15 12:49:23 +0200

    Use fieldRepository instead of regionRepository

--- src/main/java/pl/suseu/bfactions/base/field/task/FieldBarTask.java
+++ src/main/java/pl/suseu/bfactions/base/field/task/FieldBarTask.java
@@ -7,7 +7,6 @@ import pl.suseu.bfactions.base.field.Field;
 import pl.suseu.bfactions.base.field.FieldRepository;
 import pl.suseu.bfactions.base.guild.Guild;
 import pl.suseu.bfactions.base.region.Region;
-import pl.suseu.bfactions.base.region.RegionRepository;
 import pl.suseu.bfactions.base.user.User;
 import pl.suseu.bfactions.base.user.UserRepository;
 import pl.suseu.bfactions.settings.Settings;
@@ -20,24 +19,22 @@ public class FieldBarTask implements Runnable {
 
     private final BFactions plugin;
     private final FieldRepository fieldRepository;
-    private final RegionRepository regionRepository;
     private final UserRepository userRepository;
     private final Settings settings;
 
     public FieldBarTask(BFactions plugin) {
         this.plugin = plugin;
         this.fieldRepository = this.plugin.getFieldRepository();
-        this.regionRepository = this.plugin.getRegionRepository();
         this.userRepository = this.plugin.getUserRepository();
         this.settings = this.plugin.getSettings();
     }
 
     @Override
     public void run() {
-        for (Region region : regionRepository.getRegions()) {
+        for (Field field : fieldRepository.getFields()) {
+            Guild guild = field.getGuild();
+            Region region = guild.getRegion();
             double range = region.getSize() + settings.fieldBarDistance;
-            Guild guild = region.getGuild();
-            Field field = guild.getField();
 
             double progress = field.getCurrentEnergy() / field.getTier().getMaxEnergy();
             String title = String.format("%.0f (%.0f)", field.getCurrentEnergy(), progress);
