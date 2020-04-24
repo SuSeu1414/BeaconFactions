d61a8ea06120b71565fc57cb9ac5c1c679856bbb
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-04-24 20:22:14 +0200

    Disable Explosions entirely

--- src/main/java/pl/suseu/bfactions/base/field/listener/EntityExplodeListener.java
+++ src/main/java/pl/suseu/bfactions/base/field/listener/EntityExplodeListener.java
@@ -1,15 +1,11 @@
 package pl.suseu.bfactions.base.field.listener;
 
 import org.bukkit.Location;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import pl.suseu.bfactions.BFactions;
 import pl.suseu.bfactions.base.region.Region;
-
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class EntityExplodeListener implements Listener {
 
@@ -31,9 +27,10 @@ public class EntityExplodeListener implements Listener {
             region.getGuild().getField().addEnergy(-1 * plugin.getSettings().fieldDamageTNT);
             return;
         }
-        Set<Block> inRegion = event.blockList().stream()
-                .filter(block -> region.isInBorder(block.getLocation()))
-                .collect(Collectors.toSet());
-        event.blockList().removeAll(inRegion);
+        event.blockList().clear();
+//        Set<Block> inRegion = event.blockList().stream()
+//                .filter(block -> region.isInBorder(block.getLocation()))
+//                .collect(Collectors.toSet());
+//        event.blockList().removeAll(inRegion);
     }
 }
