2619fb771b7683512de03d1e8c2f0a99195f50ba
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-04-27 14:47:32 +0200

    Fix pistons being able to modify blocks from outside the field

--- src/main/java/pl/suseu/bfactions/BFactions.java
+++ src/main/java/pl/suseu/bfactions/BFactions.java
@@ -119,6 +119,7 @@ public class BFactions extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new OpenableOpenListener(this), this);
         getServer().getPluginManager().registerEvents(new TerrainModificationsListener(this), this);
         getServer().getPluginManager().registerEvents(new LiquidSpreadListener(this), this);
+        getServer().getPluginManager().registerEvents(new PistonModificationListener(this), this);
 
         getServer().getScheduler().runTaskTimerAsynchronously(this,
                 new FieldParticleTask(this), 5, 5);
