03ba2556abae3f3a5d33de2e57f9b144e1987359
verylongnicknamesuchwow <d33p4ck@gmail.com>
2020-05-04 22:49:49 +0200

    Rearrange Explosion Listeners, mob griefing enabled everywhere but active fields

--- src/main/java/pl/suseu/bfactions/base/field/listener/EntityExplodeListener.java
+++ src/main/java/pl/suseu/bfactions/base/field/listener/EntityExplodeListener.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import pl.suseu.bfactions.BFactions;
+import pl.suseu.bfactions.base.field.FieldState;
 import pl.suseu.bfactions.base.region.Region;
 
 public class EntityExplodeListener implements Listener {
@@ -17,31 +18,61 @@ public class EntityExplodeListener implements Listener {
     }
 
     @EventHandler
+    public void onBeaconBreak(EntityExplodeEvent event) {
+        Location location = event.getLocation();
+        Region region = plugin.getRegionRepository().nearestRegion(location);
+        if (region == null) {
+            return;
+        }
+        event.blockList().removeIf(block -> {
+            Location blockLocation = block.getLocation();
+            Location centerLocation = region.getCenter();
+            return blockLocation.getBlockX() == centerLocation.getBlockX()
+                    && blockLocation.getBlockY() == centerLocation.getBlockY()
+                    && blockLocation.getBlockZ() == centerLocation.getBlockZ();
+        });
+    }
+
+    @EventHandler
+    public void onTNTExplosion(EntityExplodeEvent event) {
+        Location location = event.getLocation();
+        Region region = plugin.getRegionRepository().nearestRegion(location);
+        if (region == null) {
+            return;
+        }
+        if (event.getEntityType() != EntityType.PRIMED_TNT
+                && event.getEntityType() != EntityType.MINECART_TNT) {
+            return;
+        }
+
+        //check if need to apply field damage
+        if (event.blockList().stream().anyMatch(block -> region.isInDome(block.getLocation()))) {
+            region.getGuild().getField().addEnergy(-1 * plugin.getSettings().fieldDamageTNT);
+        }
+
+        event.blockList().clear();
+    }
+
+    @EventHandler
     public void onEntityExplode(EntityExplodeEvent event) {
         Location location = event.getLocation();
         Region region = plugin.getRegionRepository().nearestRegion(location);
         if (region == null) {
             return;
         }
-        if (event.getEntityType() == EntityType.CREEPER) {
-            event.blockList().removeIf(block -> {
-                Location blockLocation = block.getLocation();
-                Location centerLocation = region.getCenter();
-                return blockLocation.getBlockX() == centerLocation.getBlockX()
-                        && blockLocation.getBlockY() == centerLocation.getBlockY()
-                        && blockLocation.getBlockZ() == centerLocation.getBlockZ();
-            });
-            return;
-        }
-        if (region.isInDome(location)) {
-            event.blockList().clear();
+
+        //check if need to apply field damage
+        if (event.blockList().stream().anyMatch(block -> region.isInDome(block.getLocation()))) {
             region.getGuild().getField().addEnergy(-1 * plugin.getSettings().fieldDamageTNT);
-            return;
         }
-        event.blockList().clear();
-//        Set<Block> inRegion = event.blockList().stream()
-//                .filter(block -> region.isInBorder(block.getLocation()))
-//                .collect(Collectors.toSet());
-//        event.blockList().removeAll(inRegion);
+
+        //remove damage in border, not in field
+        event.blockList().removeIf(block -> region.isInBorder(block.getLocation()) && !region.isInDome(block.getLocation()));
+
+        //if field enabled remove damage in field
+        if (region.getGuild().getField().getState() == FieldState.ENABLED) {
+            event.blockList().removeIf(block -> region.isInDome(block.getLocation()));
+        }
+
     }
 }
