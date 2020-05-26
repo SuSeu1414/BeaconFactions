79f1f41c5366f6a67daee9aac6c0466f723ce1bf
SuSeu1414 <szymonwitek02@gmail.com>
2020-05-26 15:03:39 +0200

    Add custom nbt tags to faction maps

--- src/main/java/pl/suseu/bfactions/map/FactionMapService.java
+++ src/main/java/pl/suseu/bfactions/map/FactionMapService.java
@@ -2,13 +2,19 @@ package pl.suseu.bfactions.map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.MapMeta;
 import org.bukkit.map.MapCanvas;
 import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import pl.suseu.bfactions.BFactions;
+
+import java.util.UUID;
 
 public class FactionMapService {
 
@@ -29,6 +35,7 @@ public class FactionMapService {
         if (mapView == null) {
             mapView = Bukkit.createMap(Bukkit.getWorlds().get(0));
         }
+//        System.out.println("id " + mapView.getId());
         mapView.setTrackingPosition(true);
         mapView.setUnlimitedTracking(true);
 //        System.out.println(mapView.getRenderers().size());
@@ -61,9 +68,27 @@ public class FactionMapService {
 //            System.out.println(renderer.getClass().toString());
 //        }
         mapMeta.setMapView(mapView);
+
+        PersistentDataContainer pdc = mapMeta.getPersistentDataContainer();
+        NamespacedKey factionMap = new NamespacedKey(plugin, "faction-map");
+        NamespacedKey random = new NamespacedKey(plugin, "random");
+        pdc.set(factionMap, PersistentDataType.BYTE, (byte) 1);
+        pdc.set(random, PersistentDataType.STRING, UUID.randomUUID().toString());
+
         itemStack.setItemMeta(mapMeta);
 
         return itemStack;
+    }
+
+    public boolean isFactionMap(ItemStack itemStack) {
+        ItemMeta im = itemStack.getItemMeta();
+        if (im == null) {
+            return false;
+        }
+
+        PersistentDataContainer pdc = im.getPersistentDataContainer();
+        NamespacedKey factionMap = new NamespacedKey(plugin, "faction-map");
+        return pdc.getOrDefault(factionMap, PersistentDataType.BYTE, (byte) 0) == 1;
     }
 
     public ItemStack getMapItem() {
